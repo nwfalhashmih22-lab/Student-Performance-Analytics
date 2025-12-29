776041dde9b7ab6bc3704815c0da9455682d9365
nwfalhashmih22-lab <nwfalhashmih22@gmail.com>
2025-12-29 18:48:39 +0400

    Create StudentPerformanceSystem.java
    this is main file

---
+++ StudentPerformanceSystem.java
@@ -0,0 +1,22 @@
+	public class StudentPerformanceAnalytics  {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	for (int i = 1; i <= 3; i++) {
+  System.out.println("Module " + i);
+	}
+	System.out.println("Enter average mark of the module:");
+	int choice = input.nextInt();
+	if (choice == 1) {
+	System.out.println("Weak Module");
+	}
+	else if (choice == 2) {
+	System.out.println("Average Module");
+	}
+	else if (choice == 3) {
+	System.out.println("Strong Module");
+	}
+	else {
+	System.out.println("Invalid mark entered");
+	}
+	}
+	}
